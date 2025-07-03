af31ed7fb529a2c728353b82676d7486120ad673
Amane-Fujiwara11 <120316618+Amane-Fujiwara11@users.noreply.github.com>
2025-07-03 23:16:18 +0900

    fix: 現状ローカルだけなので秘匿情報としない

--- backend/db/db.go
+++ backend/db/db.go
@@ -3,17 +3,16 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func NewDB() (*sql.DB, error) {
-	user := os.Getenv("DB_USER")
-	pass := os.Getenv("DB_PASS")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	name := os.Getenv("DB_NAME")
+	user := "root"
+	pass := "root"
+	host := "127.0.0.1"
+	port := "3306"
+	name := "taskdb"
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pass, host, port, name)
 	return sql.Open("mysql", dsn)
 }
